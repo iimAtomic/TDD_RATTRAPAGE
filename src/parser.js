22e81cbcfa57fd0eb7ace2cb52115b9fcc4ce75c
iimAtomic <vegbahermerite@gmail.com>
2026-09-08 13:51:23 +0200

    feat: gestion des lignes multi-segments

--- src/parser.js
+++ src/parser.js
@@ -2,3 +2,40 @@ export function parseCoordinate(str) {
   const [x, y] = str.split(',').map(Number);
   return [x, y];
 }
+
+
+export function fillSegment([x1, y1], [x2, y2]) {
+  const points = [];
+
+  if (x1 === x2) {
+    // segment vertical
+    const [yStart, yEnd] = y1 <= y2 ? [y1, y2] : [y2, y1];
+    for (let y = yStart; y <= yEnd; y++) {
+      points.push([x1, y]);
+    }
+  } else if (y1 === y2) {
+    // segment horizontal
+    const [xStart, xEnd] = x1 <= x2 ? [x1, x2] : [x2, x1];
+    for (let x = xStart; x <= xEnd; x++) {
+      points.push([x, y1]);
+    }
+  } else {
+    throw new Error('Segment diagonal non supporté');
+  }
+
+  return points;
+}
+
+export function parseLine(line) {
+  const points = line.split(' -> ').map(parseCoordinate);
+
+  const cases = [points[0]];
+
+  for (let i = 1; i < points.length; i++) {
+    const segment = fillSegment(points[i - 1], points[i]);
+    // ici j'ai pensé en vrai à retirer le premier point du segment pour éviter de dupliquer
+    cases.push(...segment.slice(1));
+  }
+
+  return cases;
+}
