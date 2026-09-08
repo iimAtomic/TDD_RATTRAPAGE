0da8acd38e41e2188f8cb6c748865075bd75eaca
iimAtomic <vegbahermerite@gmail.com>
2026-09-08 13:46:54 +0200

    test: ajout du test des segments horizontaux

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -1,8 +1,25 @@
 import { describe, it, expect } from 'vitest';
 import { parseCoordinate } from '../src/parser.js';
+import { parseCoordinate, fillSegment } from '../src/parser.js';
 
 describe('parser', () => {
-  it('transforme une coordonnée', () => {
+  it('transforme un coordonnée', () => {
     expect(parseCoordinate('498,4')).toEqual([498, 4]);
   });
+});
+
+it('remplit un segment vertical', () => {
+  expect(fillSegment([498, 4], [498, 6])).toEqual([
+    [498, 4],
+    [498, 5],
+    [498, 6],
+  ]);
+});
+
+it('remplit un segment horizontal', () => {
+  expect(fillSegment([498, 6], [496, 6])).toEqual([
+    [496, 6],
+    [497, 6],
+    [498, 6],
+  ]);
 });
